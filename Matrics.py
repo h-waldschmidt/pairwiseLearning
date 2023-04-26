cd1596445f79de7ff4ab72a5937d2b2505fad9fd
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2023-04-26 15:25:13 +0200

    fixes to make test framework runnable

--- Matrics.py
+++ Matrics.py
@@ -3,81 +3,97 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy import spatial
 
+
 def DCG(truth):
     n = len(truth)
     res = 0
     for i in range(n):
-        res += (2**truth[i] - 1.) / (math.log(i+2., 2.))
-    if (n < 1):
-        return 0.
+        res += (2 ** truth[i] - 1.0) / (math.log(i + 2.0, 2.0))
+    if n < 1:
+        return 0.0
     return float(res) / n
+
 
 def NDCG(truth):
     dcg = DCG(truth)
     perfect_DCG = DCG(sorted(truth, key=lambda x: -x))
-    if (perfect_DCG < 1e-19):
-        return 0.
+    if perfect_DCG < 1e-19:
+        return 0.0
     return dcg / perfect_DCG
+
 
 def N_correct_pairs(truth):
     correct_pairs = 0
     n_pairs = 0
     for i in range(len(truth)):
-        for j in range(i+1, len(truth)):
-            if (truth[i] != truth[j]):
+        for j in range(i + 1, len(truth)):
+            if truth[i] != truth[j]:
                 n_pairs += 1
-                if (truth[i] > truth[j]):
+                if truth[i] > truth[j]:
                     correct_pairs += 1
     n_pairs += 1e-10
-    return float(correct_pairs)/ n_pairs
+    return float(correct_pairs) / n_pairs
+
 
 def Precision(truth, n_points):
-    if (len(truth) < n_points):
+    if len(truth) < n_points:
         return 0
     else:
         return sum(truth[:n_points]) / float(n_points)
 
+
 def receive_answer(user, item, min_rating, max_rating, item_bias):
     u_answers = np.rint(np.dot(item, user) + item_bias)
-    if (u_answers < min_rating): u_answers = min_rating
-    if (u_answers > max_rating): u_answers = max_rating
+    if u_answers < min_rating:
+        u_answers = min_rating
+    if u_answers > max_rating:
+        u_answers = max_rating
     return u_answers
 
+
 def trueAnswer(ratings, item1, item2):
-    if (ratings[item1] > ratings[item2]):
+    if ratings[item1] > ratings[item2]:
         return 1
-    if (ratings[item1] < ratings[item2]):
+    if ratings[item1] < ratings[item2]:
         return -1
     return 0
 
+
 def UpdateUser(user_estim, question, bias, answer, learning_rate=0.1):
+    learning_rate = 0.1
     prediction = np.dot(user_estim, question) + bias
-    e = (answer - prediction)
-    user_estim += learning_rate * (e * question - \
-                                     0.001 * user_estim)
+    e = answer - prediction
+    user_estim += learning_rate * (e * question - 0.001 * user_estim)
 
     return user_estim
-def BestItem(items, user, used_items, items_bias, threshold = 1.):
-    #print(items.shape, user.shape)
+
+
+def BestItem(items, user, used_items, items_bias, threshold=1.0):
+    # print(items.shape, user.shape)
     items_bias_ = np.array(items_bias)
     items_bias_[items_bias > threshold] = -1000
     items_bias_[items_bias < -threshold] = -1000
     items_bias_[used_items] = -1000
-     
+
     preferences = np.dot(items, user) + items_bias_
     return np.argmax(preferences)
 
     best_item = 0
     preference = -1000
     for i in range(len(items)):
-        if i not in used_items and items_bias[i] < threshold and items_bias[i] > -1 * threshold:
+        if (
+            i not in used_items
+            and items_bias[i] < threshold
+            and items_bias[i] > -1 * threshold
+        ):
             estim_preferense = np.dot(items[i], user)
             estim_preferense += items_bias[i]
-            if (estim_preferense > preference):
+            if estim_preferense > preference:
                 del_ = items[i]
                 preference = estim_preferense
                 best_item = i
     return best_item
+
 
 def GetRecommendetList(items, user_estim, items_bias, user):
     user_estim.reshape([user_estim.shape[0], 1])
@@ -86,9 +102,10 @@ def GetRecommendetList(items, user_estim, items_bias, user):
     item_list = np.argsort(-truth)
     real_truth = np.dot(user, items.T) + items_bias
     real_truth = np.rint(real_truth)
-    real_truth[real_truth > 1] = 1.
-    real_truth[real_truth < -1]  = -1.
+    real_truth[real_truth > 1] = 1.0
+    real_truth[real_truth < -1] = -1.0
     return item_list, real_truth[item_list]
+
 
 def GetSERP(ratings):
     ratings_ = np.argsort(ratings)
@@ -107,51 +124,86 @@ def GetSERP(ratings):
     res = np.append(ones, zeros)
     return res
 
-def UserEstimation(questions_items, items, items_bias, user, user_estim, n_q, learning_rate, ratings, mode):
+
+def UserEstimation(
+    questions_items,
+    items,
+    items_bias,
+    user,
+    user_estim,
+    n_q,
+    learning_rate,
+    ratings,
+    mode,
+):
     questions_items1 = questions_items.T
     questions = (items[questions_items1[0]] - items[questions_items1[1]])[:n_q]
     bias = (items_bias[questions_items1[0]] - items_bias[questions_items1[1]])[:n_q]
-    
+
     answers = np.dot(questions, user)
-    
+
     answers += bias
     answers = np.rint(answers)
-    
 
     user_estim1 = user_estim.copy()
     for a in range(len(questions)):
         answer = answers[a]
-        if (mode == 1):
+        if mode == 1:
             answer = trueAnswer(ratings, questions_items[a][0], questions_items[a][1])
-        user_estim1 = UpdateUser(user_estim1, questions[a], bias[a], answer, learning_rate)
-    #answers[answers > 2] = 2.
-    #answers[answers < -2] = -2.
-    #answers -= bias
-   
-    #inv_questions = np.linalg.inv(np.dot(questions.T, questions) + 0.0000000000001 * np.eye(questions.shape[1]))
-    #return np.dot(inv_questions,
+        user_estim1 = UpdateUser(
+            user_estim1, questions[a], bias[a], answer, learning_rate
+        )
+    # answers[answers > 2] = 2.
+    # answers[answers < -2] = -2.
+    # answers -= bias
+
+    # inv_questions = np.linalg.inv(np.dot(questions.T, questions) + 0.0000000000001 * np.eye(questions.shape[1]))
+    # return np.dot(inv_questions,
     #                    np.dot(answers, questions))
-    #return np.dot(np.linalg.pinv(questions, 1e-3), answers)
+    # return np.dot(np.linalg.pinv(questions, 1e-3), answers)
     return user_estim1
 
-def Test(questions_items, n_q, items, items_bias, user, user_estim, ratings, all_items, all_items_bias, train_ratings, learning_rate=0.1, mode = 0):
-    if(n_q==0):
+
+def Test(
+    questions_items,
+    n_q,
+    items,
+    items_bias,
+    user,
+    user_estim,
+    ratings,
+    all_items,
+    all_items_bias,
+    train_ratings,
+    learning_rate=0.1,
+    mode=0,
+):
+    if n_q == 0:
         user_estim = user
-    if(n_q > 1):
-        user_estim = UserEstimation(questions_items, all_items, all_items_bias, user, user_estim, n_q-1, learning_rate, train_ratings, mode)
+    if n_q > 1:
+        user_estim = UserEstimation(
+            questions_items,
+            all_items,
+            all_items_bias,
+            user,
+            user_estim,
+            n_q - 1,
+            learning_rate,
+            train_ratings,
+            mode,
+        )
     # test all
-    my_recommendation_list, truth = GetRecommendetList(items, user_estim, items_bias, user)
+    my_recommendation_list, truth = GetRecommendetList(
+        items, user_estim, items_bias, user
+    )
     a = ratings[my_recommendation_list]
     dist = spatial.distance.cosine(user, user_estim)
-    #evcl_dist = spatial.distance.euclidean(user, user_estim)
-    if (mode == 1):
+    # evcl_dist = spatial.distance.euclidean(user, user_estim)
+    if mode == 1:
         truth = ratings[my_recommendation_list]
-    NDCG_ = NDCG((truth + 1) / 2.)
-    precision10 = Precision((truth + 1) / 2., 10)
+    NDCG_ = NDCG((truth + 1) / 2.0)
+    precision10 = Precision((truth + 1) / 2.0, 10)
     correct_pairs = N_correct_pairs(truth)
     return dist, NDCG_, correct_pairs, precision10
-    #print(NDCG_)
-    #return NDCG_ ,0 , 0
-
-
-
+    # print(NDCG_)
+    # return NDCG_ ,0 , 0
--- dataUtils.py
+++ dataUtils.py
@@ -1,8 +1,9 @@
 from random import gauss
 import numpy as np
 import math
-#import scipy.stats as stats
-#from sklearn.cluster import KMeans
+
+# import scipy.stats as stats
+# from sklearn.cluster import KMeans
 
 
 def SUCSESS():
@@ -16,40 +17,61 @@ def recieveAnswer(user, user_bias, item, item_bias, global_bias, sigma):
 
 def GetSimilarUsers(user, user_bias, users_estimation, user_bias_estimation):
     n_users = users_estimation.shape[0]
-    distance_to_users = [[np.dot(user - users_estimation[i], (user - users_estimation[i]).T) +
-                          (user_bias - user_bias_estimation[i])**2, i]
-                          for i in range(n_users)]
-    #distance_to_users = [[np.dot(user, users_estimation[i].T) +
+    distance_to_users = [
+        [
+            np.dot(user - users_estimation[i], (user - users_estimation[i]).T)
+            + (user_bias - user_bias_estimation[i]) ** 2,
+            i,
+        ]
+        for i in range(n_users)
+    ]
+    # distance_to_users = [[np.dot(user, users_estimation[i].T) +
     #                      (user_bias - user_bias_estimation[i]) ** 2, i]
     #                     for i in range(n_users)]
-    distance_to_users.sort(key=lambda x:x[0])
+    distance_to_users.sort(key=lambda x: x[0])
     return distance_to_users
 
-def GetReward(distance_to_users, users, users_bias, item, item_bias, global_bias, sigma):
+
+def GetReward(
+    distance_to_users, users, users_bias, item, item_bias, global_bias, sigma
+):
     n_users = 0
     result = 0
     i = 0
     while i < users.shape[0] and distance_to_users[i][0] < 0.01:
         n_users += 1
-        result += int(recieveAnswer(users[distance_to_users[i][1]], users_bias[distance_to_users[i][1]],
-                                   item, item_bias, global_bias, sigma)
-                     > SUCSESS())
+        result += int(
+            recieveAnswer(
+                users[distance_to_users[i][1]],
+                users_bias[distance_to_users[i][1]],
+                item,
+                item_bias,
+                global_bias,
+                sigma,
+            )
+            > SUCSESS()
+        )
         i += 1
-    #print('n_users = ', n_users)
+    # print('n_users = ', n_users)
     return result / float(n_users)
+
 
 def GetData(datadir):
     item_vecs = np.genfromtxt(datadir + "/items1.txt")
     item_bias = np.genfromtxt(datadir + "/items_bias1.txt")
     user_vecs_test = np.genfromtxt(datadir + "/users.txt")
-    user_vecs_train = np.genfromtxt(datadir + "/users_train.txt")
-    user_bias_train = np.genfromtxt(datadir + "/user_bias_train.txt")
-    user_bias_test = np.genfromtxt(datadir + "/user_bias.txt")
-    global_bias = 0.
-    with open(datadir + "/global_bias.txt", 'r') as global_b:
-        for line in global_b:
-            global_bias = float(line.strip())
-    return item_vecs, item_bias, user_vecs_test, user_bias_test, global_bias, user_vecs_train, user_bias_train
+    user_vecs_train = np.genfromtxt(datadir + "/users.txt")
+    # user_bias_train = np.genfromtxt(datadir + "/user_bias_train.txt")
+    # user_bias_test = np.genfromtxt(datadir + "/user_bias.txt")
+    # global_bias = 0.0
+    # with open(datadir + "/global_bias.txt", "r") as global_b:
+    #   for line in global_b:
+    #      global_bias = float(line.strip())
+    return item_vecs, item_bias, user_vecs_test, user_vecs_train
+
+
+# , , user_bias_test, global_bias, , user_bias_train
+
 
 def ExpandData(user_vecs, user_bias, expand):
     n_users = user_vecs.shape[0]
@@ -70,24 +92,31 @@ def appendAllArrays(arrays):
 
 
 def make_input(item_vec, item_bias, user_vec, user_bias):
-    return appendAllArrays([item_vec, item_bias, user_vec, user_bias, item_vec * user_vec])
-    #return item_vec * user_vec
+    return appendAllArrays(
+        [item_vec, item_bias, user_vec, user_bias, item_vec * user_vec]
+    )
+    # return item_vec * user_vec
 
 
-
-
-def OneStep(user, user_bias, item, item_bias, global_bias, r, learning_rate, user_bias_reg, user_fact_reg):
+def OneStep(
+    user,
+    user_bias,
+    item,
+    item_bias,
+    global_bias,
+    r,
+    learning_rate,
+    user_bias_reg,
+    user_fact_reg,
+):
     prediction = global_bias + user_bias + item_bias
     prediction += np.dot(user, item)
-    e = (r - prediction)  # error
+    e = r - prediction  # error
 
     # Update biases
-    user_bias += learning_rate * \
-                         (e - user_bias_reg * user_bias)
+    user_bias += learning_rate * (e - user_bias_reg * user_bias)
     # Update latent factors
-    user += learning_rate * \
-                            (e * item - \
-                             user_fact_reg * user)
+    user += learning_rate * (e * item - user_fact_reg * user)
     return [user, user_bias]
 
 
@@ -95,54 +124,84 @@ def GetBestItem(item_vecs, item_bias, user, user_bias, W, used_items):
     y1_arg = 0
     element = -1000
     for item1 in range(item_vecs.shape[0]):
-        c = np.dot(W, make_input(item_vecs[item1], item_bias[item1],
-                                                        user, user_bias))
-        if (element < c and not item1 in used_items):
+        c = np.dot(W, make_input(item_vecs[item1], item_bias[item1], user, user_bias))
+        if element < c and not item1 in used_items:
             element = c
             y1_arg = item1
     return y1_arg
+
 
 def SortIItemByPopularity(users, users_bias, item, item_bias, global_bias, sigma):
     result = []
     n_users = users.shape[0]
     for i in range(item.shape[0]):
-        result.append([
-            sum(float(recieveAnswer(users[j], users_bias[j], item[i], item_bias[i], global_bias, sigma) > SUCSESS())
-                for j in range(users.shape[0])) / n_users ,
-            item_bias[i] , i])
-    result.sort(key = lambda x:x[0])
+        result.append(
+            [
+                sum(
+                    float(
+                        recieveAnswer(
+                            users[j],
+                            users_bias[j],
+                            item[i],
+                            item_bias[i],
+                            global_bias,
+                            sigma,
+                        )
+                        > SUCSESS()
+                    )
+                    for j in range(users.shape[0])
+                )
+                / n_users,
+                item_bias[i],
+                i,
+            ]
+        )
+    result.sort(key=lambda x: x[0])
     return result
 
+
 def learning_rate(step):
-    return 1./math.sqrt(step)
+    return 1.0 / math.sqrt(step)
+
 
 def GetItemsNames(file):
     res = {}
     with open(file) as f:
         for line in f:
             try:
-                line = line.split(',')
+                line = line.split(",")
                 res[int(line[0])] = [line[1], line[-1]]
             except:
                 continue
     return res
 
+
 def PrintItemPopularity(user_vecs, user_bias, item_vecs, item_bias, global_bias, sigma):
-    sort_items = SortIItemByPopularity(user_vecs, user_bias, item_vecs, item_bias, global_bias, sigma)
+    sort_items = SortIItemByPopularity(
+        user_vecs, user_bias, item_vecs, item_bias, global_bias, sigma
+    )
     items_test = np.genfromtxt("data/test_items.txt")
     items_names = GetItemsNames("../dataset/ml-1m/movies_mine.dat")
     ratings = []
     ratings1 = []
-    with open("itemPopularity", 'w') as ip:
+    with open("itemPopularity", "w") as ip:
         for i in sort_items:
             try:
-                if (float(items_names[items_test[i[2]] + 1][1]) > 0):
-                    #print(i[-1], items_names[items_test[i[-1]] + 1])
-                    ip.write(str(i[0]) + "\t" + str(i[1]) + "\t"  + str(items_test[i[2]]) +
-                         "\t" + items_names[items_test[i[2]] + 1][0] +
-                         "\t" + items_names[items_test[i[2]] + 1][1].strip() +
-                         '\n')
-                    ratings.append(float(items_names[items_test[i[2]] + 1][1]) / 10.)
+                if float(items_names[items_test[i[2]] + 1][1]) > 0:
+                    # print(i[-1], items_names[items_test[i[-1]] + 1])
+                    ip.write(
+                        str(i[0])
+                        + "\t"
+                        + str(i[1])
+                        + "\t"
+                        + str(items_test[i[2]])
+                        + "\t"
+                        + items_names[items_test[i[2]] + 1][0]
+                        + "\t"
+                        + items_names[items_test[i[2]] + 1][1].strip()
+                        + "\n"
+                    )
+                    ratings.append(float(items_names[items_test[i[2]] + 1][1]) / 10.0)
                     ratings1.append(i[0])
             except:
                 continue
@@ -152,41 +211,43 @@ def PrintItemPopularity(user_vecs, user_bias, item_vecs, item_bias, global_bias,
     ratings /= np.max(ratings)
     a1 = np.arange(ratings.shape[0])
     a = np.argsort(ratings)
-    print(np.corrcoef(ratings,ratings1)[0][1])
-    return  np.corrcoef(ratings,ratings1)[0][1]
+    print(np.corrcoef(ratings, ratings1)[0][1])
+    return np.corrcoef(ratings, ratings1)[0][1]
+
 
 def GetItemPopularity():
     result = []
-    with open("itemPopularity", 'r') as ip:
+    with open("itemPopularity", "r") as ip:
         for line in ip:
             line = int(line.strip().split("\t")[2])
             result.append(line)
     return list(reversed(result))
 
+
 def DeleteMostPopularItems(n_delete_items):
     items_pop = GetItemPopularity()
-    with open("data/items.txt") as items, \
-         open("data/items_bias.txt") as i_b, \
-         open("data/items1.txt", 'w') as n_items,\
-         open("data/items_bias1.txt", 'w') as n_ib:
+    with open("data/items.txt") as items, open("data/items_bias.txt") as i_b, open(
+        "data/items1.txt", "w"
+    ) as n_items, open("data/items_bias1.txt", "w") as n_ib:
         n_line = 0
         for line in items:
             if n_line not in items_pop[:n_delete_items]:
                 n_items.write(line)
             else:
-                n_items.write('\t'.join(['0' for i in range(10)]) + '\n')
+                n_items.write("\t".join(["0" for i in range(10)]) + "\n")
             n_line += 1
         n_line = 0
         for line in i_b:
             if n_line not in items_pop[:n_delete_items]:
                 n_ib.write(line)
             else:
-                n_ib.write('0' + '\n')
+                n_ib.write("0" + "\n")
             n_line += 1
+
 
 def GetNRatingsOfItems():
     result = {}
-    with open('data/items_ratings.txt') as f:
+    with open("data/items_ratings.txt") as f:
         for line in f:
             line = line.strip().split()
             result[int(line[0])] = int(line[1])
@@ -196,9 +257,9 @@ def GetNRatingsOfItems():
 def DeleteItemsWithoutManyRatings(threshold):
     itemsRatings = GetNRatingsOfItems()
     items = open("data/items.txt").readlines()
-    items1 = open("data/items1.txt", 'w')
+    items1 = open("data/items1.txt", "w")
     items_b = open("data/items_bias.txt").readlines()
-    items_b1 = open("data/items_bias1.txt", 'w')
+    items_b1 = open("data/items_bias1.txt", "w")
     dictionaryToItems = {}
     newline_n = 0
     for line_n, line in enumerate(items):
@@ -207,22 +268,24 @@ def DeleteItemsWithoutManyRatings(threshold):
             items1.write(line)
             items_b1.write(items_b[line_n])
             newline_n += 1
-    with open('itemPopularity') as iP, open('data/itemPopularity', 'w') as nIP:
+    with open("itemPopularity") as iP, open("data/itemPopularity", "w") as nIP:
         for line in iP:
-            line = line.strip().split('\t')
-            if (itemsRatings[int(line[2])] > threshold):
+            line = line.strip().split("\t")
+            if itemsRatings[int(line[2])] > threshold:
                 line[2] = str(dictionaryToItems[int(line[2])])
-                nIP.write('\t'.join(line) + '\n')
+                nIP.write("\t".join(line) + "\n")
 
 
 def VectorToString(vector):
     return "_".join(str(v) for v in vector)
 
+
 def LearningRate(learning_rate, step):
     res = learning_rate / step
-    if (res < 0.01):
+    if res < 0.01:
         res = 0.01
     return res
+
 
 def GetDistributionOfNorms():
     item_vecs, item_bias, user_vecs, user_bias, global_bias = GetData("data")
@@ -233,10 +296,10 @@ def GetDistributionOfNorms():
     norms = np.array(norms)
     np.savetxt("data/item_norms", norms)
 
+
 def GetInverseMatrix(previousMatrix, new_vector):
     v = np.dot(previousMatrix, new_vector)
-    return previousMatrix - \
-    (np.dot(previousMatrix, v) / (1. + np.trace(v)))
+    return previousMatrix - (np.dot(previousMatrix, v) / (1.0 + np.trace(v)))
 
 
 class StateStat(object):
@@ -244,12 +307,22 @@ class StateStat(object):
         self.nq = 0
         self.npositiv = 0
 
+
 def ClusterItems():
-    item_vecs, item_bias, user_vecs, user_bias, global_bias, user_vecs_train, user_bias_train = GetData("data")
+    (
+        item_vecs,
+        item_bias,
+        user_vecs,
+        user_bias,
+        global_bias,
+        user_vecs_train,
+        user_bias_train,
+    ) = GetData("data")
     kmeans = KMeans(n_clusters=50).fit(item_vecs)
     np.savetxt("data/item_clusters", kmeans.cluster_centers_)
 
-#ClusterItems()
-#item_vecs, item_bias, user_vecs, user_bias, global_bias, user_vecs_train, user_bias_train = GetData("data")
-#print(item_vecs.shape)
-#PrintItemPopularity(user_vecs_train, user_bias_train, item_vecs, item_bias, global_bias, 0.001)
+
+# ClusterItems()
+# item_vecs, item_bias, user_vecs, user_bias, global_bias, user_vecs_train, user_bias_train = GetData("data")
+# print(item_vecs.shape)
+# PrintItemPopularity(user_vecs_train, user_bias_train, item_vecs, item_bias, global_bias, 0.001)
